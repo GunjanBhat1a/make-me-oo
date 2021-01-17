81772ade324b44db090e806845c05557c94513d8
GunjanBhat1a <gunjan.bhatia@thoughtworks.com>
2021-01-17 15:45:27 +0530

    Fix the code according OOPs, change done : Refactor test file according to AAA format. Rename test file. Delete DistanceAndDirectionCalculator class. Added behaviour and properties in Point class.

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,24 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    private double xDifference(Point from) {
+        return this.x - from.x;
+    }
+
+    private double yDifference(Point from) {
+        return this.y - from.y;
+    }
+
+    public double distanceBetween(Point from) {
+        return Math.sqrt(Math.pow(xDifference(from), 2) + Math.pow(yDifference(from), 2));
+    }
+
+    public double directionOf(Point from) {
+        return Math.atan2(yDifference(from), xDifference(from));
     }
 }
